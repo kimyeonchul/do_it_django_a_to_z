780e54c33eead486b5166bbbd84e7f509aa9d7f5
kimyeonchul <yeonchul0807@nate.com>
2023-03-21 15:21:49 +0900

    post에 updated_at 추가

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,8 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     # author
 
     def __str__(self):
