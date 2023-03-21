428561f0da8a4cce734f57f607ebeea7bacf2cc9
kimyeonchul <yeonchul0807@nate.com>
2023-03-21 17:10:21 +0900

    FBV를 CBV로 변경해보기, URL 패턴 매칭

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,33 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView,DetailView
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts':posts
-        }
-    )
-def single_post_page(request,pk):
-    post = Post.objects.get(pk=pk)
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post':post
-        }
-    )
+
+class PostList(ListView):
+    model=Post
+    ordering = '-pk'
+    template_name = 'blog/index.html'
+
+class PostDetail(DetailView):
+    model=Post
+    template_name = 'blog/post_detail.html'
+
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts':posts
+#         }
+#     )
+# def single_post_page(request,pk):
+#     post = Post.objects.get(pk=pk)
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post':post
+#         }
+#     )
 
