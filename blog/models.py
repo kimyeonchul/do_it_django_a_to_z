5db98379e196ab5e6a690fe3ceff8b02ae81ca6f
kimyeonchul <yeonchul0807@nate.com>
2023-03-28 17:24:52 +0900

    첨부파일 다운로드 기능, 이미지 없는 글 처리 기능 추가

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,5 @@
+import os.path
+
 from django.db import models
 
 class Post(models.Model):
@@ -16,3 +18,6 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
