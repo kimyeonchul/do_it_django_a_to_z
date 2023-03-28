7586eb7cef79e9d51a1b157632bbc29eed6e9704
kimyeonchul <yeonchul0807@nate.com>
2023-03-28 17:04:14 +0900

    Add Static files, 파일 첨부, 이미지 첨부

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,8 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/',blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/',blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # author
